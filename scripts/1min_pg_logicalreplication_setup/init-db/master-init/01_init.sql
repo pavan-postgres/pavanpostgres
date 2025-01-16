9a78b94da31d60f967efa28511f3ef51acfbf7f8
PavanTeja <35553713+pavan-postgres@users.noreply.github.com>
2025-01-16 23:46:59 +0530

    Create 01_init.sql
    Initial script to be executed on master

---
+++ scripts/1min_pg_logicalreplication_setup/init-db/master-init/01_init.sql
@@ -0,0 +1,30 @@
+-- create replica user
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'replica_user') THEN
+        CREATE ROLE replica_user WITH LOGIN REPLICATION PASSWORD 'postgres';
+    END IF;
+END;
+$$;
+
+
+
+-- Create database if it doesn't exist (for safety)
+ SELECT 'CREATE DATABASE testDB' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'testDB')\gexec ;
+
+-- Connect to testDB (this is necessary because we might be running in the 'postgres' DB by default)
+\c testDB
+
+-- Create the orders table if it doesn't exist
+CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    order_date DATE NOT NULL
+);
+
+-- grant privilege on orders table to replica_user
+
+GRANT SELECT ON TABLE orders to replica_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO replica_user;
